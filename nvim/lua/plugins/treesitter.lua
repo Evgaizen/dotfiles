e024afa29229e7ecbcdec7c9eec56b917c5e0819
Nikita Ivanov <49843824+Evgaizen@users.noreply.github.com>
2025-02-16 02:01:15 +0300

    feat(nvim): Add treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		build = ":TSUpdate",
+		lazy = vim.fn.argc(-1) == 0,
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				"html",
+				"javascript",
+				"json",
+				"jsdoc",
+				"lua",
+				"markdown",
+				"tsx",
+				"typescript",
+				"yaml"
+			}
+		}
+	}
+}
