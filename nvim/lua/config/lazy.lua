fee2eccc3a5fe42d6d5fa3ff58578273475ebe4b
Nikita Ivanov <49843824+Evgaizen@users.noreply.github.com>
2025-02-16 21:11:17 +0300

    feat(nvim): Add nvim options config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,8 +16,7 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+require("config.options")
 
 require("lazy").setup({
 	spec = {
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.title = true
+vim.opt.shell = "zsh"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false
