7e029b1159b6802ebc3eb19f95f20ce292065420
Nikita Ivanov <49843824+Evgaizen@users.noreply.github.com>
2025-03-06 02:01:09 +0300

    feat(nvim): Add mapping for autocomplete

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -2,6 +2,7 @@ return {
 	{ "jiangmiao/auto-pairs" },
 	{
 		"hrsh7th/nvim-cmp",
+		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-cmdline",
@@ -9,9 +10,25 @@ return {
 		},
 		opts = function()
 			local cmp = require('cmp')
+			local select_opts = {behavior = cmp.SelectBehavior.Select}
 
 			return {
-				mappings = cmp.mapping.preset.insert(),
+				mapping = cmp.mapping.preset.insert({
+					['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+    			['<Down>'] = cmp.mapping.select_next_item(select_opts),
+					['<CR>'] = cmp.mapping.confirm({select = false}),
+					['<Tab>'] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								local entry = cmp.get_selected_entry()
+								if not entry then
+									cmp.select_next_item(select_opts)
+								end
+								cmp.confirm()
+							else
+								fallback()
+							end
+						end),
+				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" }
 				}, {
